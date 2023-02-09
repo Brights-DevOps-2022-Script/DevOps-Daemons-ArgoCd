484c4b35fd9cd2176ff2690235156f4dd72e4b5c
Gurki73 <81249774+Gurki73@users.noreply.github.com>
2023-02-09 07:30:56 +0100

    added basic functions

--- BashScripts/updateImage.sh
+++ BashScripts/updateImage.sh
@@ -2,7 +2,8 @@ function update_config () {
   rm ../App/version.config;
   cp ../App/temp.file ../App/version.config
   rm ../App/temp.file
-  return 1
+  push_to_git
+  return 0
 }
 
 function create_temp () {
@@ -35,16 +36,15 @@ function check_config () {
     fi
 }
 
-function increment_version_segment () {
-then 
+function increment_version_segment () { 
     create_temp
     file="../App/version.config"
     while read line; do
-        if  [[ ${line} = $1* ]]
+        if  [[ ${line} = $4* ]]
         then
             value=$(echo ${line} | awk -F: '{print $2}')
             value=$((value + 1))
-            echo ${1}:${value}  >> ../App/temp.file
+            echo ${4}:${value}  >> ../App/temp.file
         else 
         echo ${line} >> ../App/temp.file
         fi
@@ -52,17 +52,16 @@ then
     update_config
     return 0
 fi
-return 1
+exit
 }
 
-function increment_version_build () {
-then 
+function set_version_build () {
     create_temp
     file="../App/version.config"
     while read line; do
         if  [[ ${line} = Build* ]]
         then
-            echo Build:$2  >> ../App/temp.file
+            echo Build:$5  >> ../App/temp.file
         else 
         echo ${line} >> ../App/temp.file
         fi
@@ -74,7 +73,18 @@ return 1
 }
 
 function push_to_git () {
-  git pull
-  git add ../App/version.config
-  git push
+    git pull https://$1:$2@$3 HEAD:main
+    git checkout main
+    git add ../App/config.yaml
+    git commit -m 'updatet version ${4}'
+    git push https://$2:$3@$3 HEAD:main
 }
+
+changes=$(git diff HEAD^ --name-only ../App)
+if [ -n "$changes" ]; then
+  if [ "$4" = "Build" ]; then
+    set_version_build "$@"
+  else
+    increment_version_segment "$@"
+  fi
+fi
