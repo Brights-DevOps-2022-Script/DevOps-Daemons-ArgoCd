c5562fc890d0b21fdffd668da628beb0e7332b5c
FluffynatorT800 <keineidee1@gmx.de>
2023-02-16 13:29:23 +0100

    modified kustomize via bashscrpst

--- BashScripts/deployFile1.sh
+++ BashScripts/deployFile1.sh
@@ -3,6 +3,8 @@ echo "apiVersion: kustomize.config.k8s.io/v1beta1"    >  ./yml-Files/kustomizati
 echo "kind: Kustomization"                            >> ./yml-Files/kustomization.yml
 echo "resources:"                                     >> ./yml-Files/kustomization.yml
 echo "  - allinone.yml"                               >> ./yml-Files/kustomization.yml
+echo "  - nginx_config_map.yml"                       >> ./yml-Files/kustomization.yml
+echo "  - service_monitor.yml"                        >> ./yml-Files/kustomization.yml
 echo "images:"                                        >> ./yml-Files/kustomization.yml
 echo "   - name: nginxcomic"                          >> ./yml-Files/kustomization.yml
 echo "     newName: devops2022.azurecr.io/${1}"       >> ./yml-Files/kustomization.yml
